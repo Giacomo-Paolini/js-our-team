ad3875eb596ad087bf0591410fe92c753a00982c
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-05-02 23:04:28 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 let team = [
-    {nome: 'Wayne Barnett', ruolo: 'Founder & CEO', foto: 'wayne-barnett-founder-ceo.jpg'},
-    {nome: 'Angela Caroll', ruolo: 'Chief Editor', foto: 'angela-caroll-chief-editor.jpg'},
-    {nome: 'Walter Gordon', ruolo: 'Office Manager', foto: 'walter-gordon-office-manager.jpg'},
-    {nome: 'Angela Lopez', ruolo: 'Social Media Manager', foto: 'angela-lopez-social-media-manager.jpg'},
-    {nome: 'Scott Estrada', ruolo: 'Developer', foto: 'scott-estrada-developer.jpg'},
-    {nome: 'Barbara Ramos', ruolo: 'Graphic Designer', foto: 'barbara-ramos-graphic-designer.jpg'}
+    {nome: 'Wayne Barnett', ruolo: 'Founder & CEO', foto: './img/wayne-barnett-founder-ceo.jpg'},
+    {nome: 'Angela Caroll', ruolo: 'Chief Editor', foto: './img/angela-caroll-chief-editor.jpg'},
+    {nome: 'Walter Gordon', ruolo: 'Office Manager', foto: './img/walter-gordon-office-manager.jpg'},
+    {nome: 'Angela Lopez', ruolo: 'Social Media Manager', foto: './img/angela-lopez-social-media-manager.jpg'},
+    {nome: 'Scott Estrada', ruolo: 'Developer', foto: './img/scott-estrada-developer.jpg'},
+    {nome: 'Barbara Ramos', ruolo: 'Graphic Designer', foto: './img/barbara-ramos-graphic-designer.jpg'}
 ]
-// console.log(team);
-
 for (let i = 0; i < team.length; i++) {
     let teamContainer = document.getElementById("teamContainer");
     let cards = document.createElement("div");
-    cards.classList.add("card")
+    cards.classList.add("card");
     let member = team[i];
-    cards.innerText = member[i];
+    cards.innerHTML = `<img src="${member.foto}" class="card-img-top">
+    <h3>${member.nome}</h3>
+    <p>${member.ruolo}</p>`;
     teamContainer.appendChild(cards);
-    console.log(member)
+    console.log(member);
 }
